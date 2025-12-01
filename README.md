9e2cba6e750991041b2e60e249aef4093b31aa8d
Harrie Smulders <h.l.w.smulders@tue.nl>
2025-12-01 08:40:08 +0100

    Chapter 001 - Initial commit, empty Wagtail site

---
+++ README.md
@@ -0,0 +1,14 @@
+# archeryscoring
+
+## See also
+
+ [https://docs.wagtail.org/en/stable/getting_started/tutorial.html]
+
+  ## Chapter 001 - Initial commit, empty Wagtail site
+
+```bash
+python -m venv .venv
+pip install wagtail
+wagtail start scoring .
+```
+
---
+++ scoring/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-g&jc$=sgg6*t_kuksbo@wrbl#vg@6jlm+9a8(osv#tepwt(1l@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
